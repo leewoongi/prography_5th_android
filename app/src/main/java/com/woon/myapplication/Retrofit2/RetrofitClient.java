a647437b81f31bf0916f34dc4a78099aa5d10ad8
leewoongi <t2korea@naver.com>
2020-03-04 01:46:13 +0900

    .Manifest 인터넷 권한 추가

--- app/src/main/java/com/woon/myapplication/Retrofit2/RetrofitClient.java
+++ app/src/main/java/com/woon/myapplication/Retrofit2/RetrofitClient.java
@@ -11,10 +11,10 @@ public class RetrofitClient {
     public static RetrofitService getService() {
         if (service == null) {
             service = new Retrofit.Builder()
-                    .baseUrl("https://ghibliapi.herokuapp.com/")
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build().create(RetrofitService.class);
-        }
+                .baseUrl("https://ghibliapi.herokuapp.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build().create(RetrofitService.class);
+    }
 
         return service;
     }
